235f08078edadf629aa658f714a93b5053f43dcb
TylorShine <ricka.tylor@gmail.com>
2024-04-09 17:06:16 +0900

    fix import error in WavLM.py (local problem)

--- modules/encoders/wavlm/WavLM.py
+++ modules/encoders/wavlm/WavLM.py
@@ -17,7 +17,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import LayerNorm
-from modules import (
+from .modules import (
     Fp32GroupNorm,
     Fp32LayerNorm,
     GradMultiply,
