b4cc6ef58f6ed7e62b2a3522f0d2b775518dc72e
TylorShine <ricka.tylor@gmail.com>
2024-04-14 14:35:23 +0900

    update discriminator param

--- modules/discriminator.py
+++ modules/discriminator.py
@@ -102,12 +102,12 @@ class DiscriminatorS(torch.nn.Module):
                 norm_f(Conv1d(1, 16, 15, 1, padding=7)),
                 norm_f(Conv1d(16, 64, 41, 4, groups=4, padding=20)),
                 norm_f(Conv1d(64, 256, 41, 4, groups=16, padding=20)),
-                # norm_f(Conv1d(256, 1024, 41, 4, groups=64, padding=20)),
+                norm_f(Conv1d(256, 1024, 41, 4, groups=64, padding=20)),
                 # norm_f(Conv1d(1024, 1024, 41, 4, groups=256, padding=20)),
                 # norm_f(Conv1d(1024, 1024, 5, 1, padding=2)),
             ]
         )
-        self.conv_post = norm_f(Conv1d(256, 1, 3, 1, padding=1))
+        self.conv_post = norm_f(Conv1d(1024, 1, 3, 1, padding=1))
         
 
     def forward(self, x: torch.Tensor) -> tuple[torch.Tensor, list[torch.Tensor]]:
